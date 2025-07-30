819029a81c54b13074cc309d03d1b999278904d6
Riajul Islam <Riajul5@hotmail.co.uk>
2025-07-30 21:33:42 +0100

    added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd terraform
+
+# Delete Helm releases
+helm uninstall prometheus -n prometheus
+helm uninstall argocd -n argo-cd
+helm uninstall external-dns -n external-dns
+helm uninstall cert-manager -n cert-manager
+helm uninstall nginx-ingress -n nginx-ingress
+
+# Delete Ingress
+kubectl delete ingress game-ingress -n apps
+
+# Delete Service
+kubectl delete svc game-2048-service -n apps
+
+# Delete Deployment
+kubectl delete deployment game-deploy -n apps
